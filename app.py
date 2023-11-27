3008d6e356e51e2bb10b29e58ae738c6b7b49011
Odalis Castillo <gabrielaocm10@gmail.com>
2023-11-26 22:12:27 -0600

    Login, register y layout

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+@login_required
+def home():
+    """Website home"""
+    return render_template("home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    return render_template("register.html")
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
