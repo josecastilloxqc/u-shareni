00d71f613ef267dd936c6fee1bde570588ef268f
Jose <josecastillo33308@gmail.com>
2023-11-29 11:44:51 -0600

    update

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """Website home"""
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    
+
 
 @app.route("/home")
 def home():
@@ -27,8 +30,7 @@ def register():
     confirmacion = request.form.get("confirmation")
     correo = request.form.get("correo")
 
-
-
+    redirect ("/login.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
