9359d10566c13f9007a244d76eedb9ee38c16daa
gh0st545 <maurosantiago985@gmail.com>
2023-11-29 10:45:18 -0600

    cambios hechos hasta ahora

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Website home"""
-    return render_template("home.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
     return render_template("login.html")
+
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     return render_template("login.html")
 
 @app.route("/logout")
 def logout():
